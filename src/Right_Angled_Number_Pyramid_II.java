a81fa81b39cfdbba193b0f9dd76f96bc1839376b
Skoushik54 <140392717+Skoushik54@users.noreply.github.com>
2024-08-27 09:45:37 +0530

    Right Angled NUmber Pyramid 2 is added

---
+++ src/Right_Angled_Number_Pyramid_II.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Right_Angled_Number_Pyramid_II {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        PrintPattern(n);
+    }
+    static void PrintPattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
